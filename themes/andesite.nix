f757685c300845f179967a9bbb6faa43032b0fbf
unknown <arpeggio.gns@gmail.com>
2026-08-24 09:08:14 -0700

    feat: add theme definitions for andesite, arasaka, and king with accompanying documentation

---
+++ themes/andesite.nix
@@ -0,0 +1,59 @@
+{
+  lib,
+  pkgs,
+  config,
+  ...
+}: {
+  # Hyprland Config
+  options.theme = lib.mkOption {
+    type = lib.types.attrs;
+    default = {
+      rounding = 20;
+      bar-height = 36;
+      gaps-in = 8;
+      gaps-out = 8 * 2;
+      active-opacity = 0.96;
+      inactive-opacity = 0.92;
+      blur = true;
+      border-size = 2;
+      animation-speed = "very-fast"; # "very-fast" | "fast" | "medium" | "slow"
+    };
+    description = "Theme configuration options";
+  };
+
+  config.stylix = {
+    cursor = {
+      name = "BreezeX-RosePine-Linux";
+      package = pkgs.rose-pine-cursor;
+      size = 20;
+    };
+
+    fonts = {
+      monospace = {
+        package = pkgs.maple-mono.NF;
+        name = "Maple Mono NF";
+      };
+      sansSerif = {
+        package = pkgs.rubik;
+        name = "Rubik";
+      };
+      serif = config.stylix.fonts.sansSerif;
+      emoji = {
+        package = pkgs.noto-fonts-color-emoji;
+        name = "Noto Color Emoji";
+      };
+      sizes = {
+        applications = 13;
+        desktop = 13;
+        popups = 13;
+        terminal = 13;
+      };
+    };
+
+    polarity = "dark";
+    image = pkgs.fetchurl {
+      url = "https://raw.githubusercontent.com/anotherhadi/awesome-wallpapers/main/wallpapers/the-cpu_animated_black.gif";
+      sha256 = "sha256-peigNzQDxvDqRCz9f0PPaejiAafD1o1q8H6kVpazhRE=";
+    };
+  };
+}
